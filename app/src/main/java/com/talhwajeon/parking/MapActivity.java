d4002466d0d10c7a515fe22eaa2fe6dd26ded5ec
Lagnesia <sjkwon619@naver.com>
2018-12-01 18:45:08 +0900

    add map and marker
    add MapActivity
    add API key for Google Map

---
+++ app/src/main/java/com/talhwajeon/parking/MapActivity.java
@@ -0,0 +1,38 @@
+package com.talhwajeon.parking;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng current_loc = new LatLng(37.52487, 126.92723);
+
+        MarkerOptions markerOpt = new MarkerOptions();
+        markerOpt
+                .position(current_loc);
+
+        map.addMarker(markerOpt);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(current_loc));
+    }
+}
