e301389776fa3b078bb193e7e47ddef5abdf03e3
nwerl <31559679+nwerl@users.noreply.github.com>
2018-12-02 02:56:54 +0900

    Add files via upload

--- app/src/main/java/com/talhwajeon/parking/MainActivity.java
+++ app/src/main/java/com/talhwajeon/parking/MainActivity.java
@@ -1,7 +1,10 @@
-package com.talhwajeon.parking;
+package com.example.nwerl.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton b1 = (ImageButton)findViewById(R.id.mb1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        MyParking.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
